103139de5dbe2e942a821e5f23caf8c59ead2b78
Chris Murray <mur11020@byui.edu>
2018-10-11 16:58:54 -0600

    Setup Actor Components

---
+++ Source/FPSGame/Public/FPSObjectActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSObjectActor.generated.h"
+
+UCLASS()
+class FPSGAME_API AFPSObjectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSObjectActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	//the object
+	UStaticMeshComponent* MeshComp;
+	//used for collision
+	USphereComponent* SphereComp;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
